f4d00a4ccd1f8a609bf7fae8a963f7a8a673bfbb
uniseraph <zhengtao.wuzt@gmail.com>
2017-08-17 23:01:27 +0800

    fix bug

--- plugins/slb/start.sh
+++ plugins/slb/start.sh
@@ -2,7 +2,7 @@
 
 BASE_DIR=$(cd `dirname $0` && pwd -P)
 
-REGION=$(curl -fsL 100.100.100.200/latest/meta-data/region-id)
+REGION_ID=$(curl -fsL 100.100.100.200/latest/meta-data/region-id)
 INSTANCE_ID=$(curl -fsL  100.100.100.200/latest/meta-data/instance-id)
 
 
@@ -11,7 +11,7 @@ docker run --net=host -ti --rm \
 	    -v /var/run/docker.sock:/var/run/docker.sock \
         -e DOCKER_HOST=unix:///var/run/docker.sock  \
         -e LOCAL_IP=${LOCAL_IP} \
-        -e REGION=${REGION} \
+        -e REGION_ID=${REGION_ID} \
 	    -e ACCESS_KEY_ID=${ACCESS_KEY_ID}  \
 	    -e ACCESS_KEY_SECRET=${ACCESS_KEY_SECRET} \
 	    -e INSTANCE_ID=${INSTANCE_ID} \
