d41178d451daed4ed75be4ee9a81f093f3105b35
uniseraph <zhengtao.wuzt@gmail.com>
2017-08-24 23:55:05 +0800

    把所有系统容器都移到boostrap docker

--- plugins/watchdog/start.sh
+++ plugins/watchdog/start.sh
@@ -2,11 +2,10 @@
 
 BASE_DIR=$(cd `dirname $0` && pwd -P)
 
-docker run --net=host -ti --rm \
+docker -H unix:///var/run/bootstrap.sock run --net=host -ti --rm \
         -v ${BASE_DIR}:${BASE_DIR} \
-	    -v /var/run/docker.sock:/var/run/docker.sock \
-        -e DOCKER_HOST=unix:///var/run/docker.sock  \
-        -e LOCAL_IP=${LOCAL_IP} \
+	    -v /var/run/bootstrap.sock:/var/run/bootstrap.sock \
+        -e DOCKER_HOST=unix:///var/run/bootstrap.sock  \
         -w ${BASE_DIR} \
         docker/compose:1.9.0 \
         up -d $*
