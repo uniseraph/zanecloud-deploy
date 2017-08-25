facc5b32700c4e1b7109a2e8f3d6d0483781f056
wuzhengtao <zhengtao.wuzt@gmail.com>
2017-08-25 14:30:13 +0800

    为watchdog注入本机ip，为host 容器暴露ip需要

--- plugins/watchdog/start.sh
+++ plugins/watchdog/start.sh
@@ -6,6 +6,7 @@ docker -H unix:///var/run/bootstrap.sock run --net=host -ti --rm \
         -v ${BASE_DIR}:${BASE_DIR} \
 	    -v /var/run/bootstrap.sock:/var/run/bootstrap.sock \
         -e DOCKER_HOST=unix:///var/run/bootstrap.sock  \
+        -e LOCAL_IP=${LOCAL_IP} \
         -w ${BASE_DIR} \
         docker/compose:1.9.0 \
         up -d $*
